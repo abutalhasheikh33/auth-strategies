356b7fc88a8fbd8a27cc4bc2ad9f51714b358db0
Abutalha <abutalhasheikh33@gmail.com>
2023-12-09 01:05:29 +0530

    google-signin-added

--- firebase-auth/src/components/Form.jsx
+++ firebase-auth/src/components/Form.jsx
@@ -1,60 +1,91 @@
-import React, { useState } from 'react'
-import {getAuth,createUserWithEmailAndPassword } from "firebase/auth"
-import {redirect,useNavigate} from "react-router-dom"
+import React, { useState } from 'react';
+import { getAuth, createUserWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
 
 const Form = () => {
   const auth = getAuth();
+  const provider = new GoogleAuthProvider();
   const navigate = useNavigate();
-  const [email,setEmail] = useState("");
-  const [password,setPassword] = useState("")
-  const [user,setUser] = useState()
-  const [error,setError] = useState("")
-  const submit = (e)=>{
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState('');
+
+  const handleSignUp = (e) => {
     e.preventDefault();
     createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      // Signed up 
-      const user = userCredential.user;
-      setUser(user)
-      navigate('/welcome', { replace: true });
-      // ...
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      setError(errorMessage)
-      // ..
-    });
-  }
-  
+      .then((userCredential) => {
+        const newUser = userCredential.user;
+        setUser(newUser);
+        navigate('/welcome', { replace: true });
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
+  };
+
+  const handleSignUpWithGoogle = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        setUser(user);
+        navigate('/welcome', { replace: true });
+      })
+      .catch((error) => {
+        setError(error.message);
+        // Handle specific errors if needed
+      });
+  };
+
   return (
     <div className="w-full max-w-xs">
-      <h1 className='font-bold text-blue-700 text-xl'>Create a new user</h1>
-  <form onSubmit={(e)=>submit(e)} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-    
-    <div className="mb-4">
-      <label className="block text-gray-700 text-sm font-bold mb-2" for="username">
-        Username
-      </label>
-      <input value={email} onChange={((e)=>setEmail(e.target.value))} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
+      <h1 className="font-bold text-blue-700 text-xl text-center">Create a new user</h1>
+      <form onSubmit={(e) => handleSignUp(e)} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+            Email
+          </label>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="email"
+            type="email"
+            placeholder="Email"
+          />
+        </div>
+        <div className="mb-6">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+            Password
+          </label>
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            id="password"
+            type="password"
+            placeholder="Password"
+          />
+          <p className="text-red-500 text-xs italic">{error}</p>
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Sign Up
+          </button>
+          <button
+            onClick={handleSignUpWithGoogle}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="button"
+          >
+            Sign Up with Google
+          </button>
+        </div>
+      </form>
     </div>
-    <div className="mb-6">
-      <label className="block text-gray-700 text-sm font-bold mb-2" for="password">
-        Password
-      </label>
-      <input value={password} onChange={((e)=>setPassword(e.target.value))} className="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" />
-      <p className="text-red-500 text-xs italic">{error}</p>
-    </div>
-    <div className="flex items-center justify-between">
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-        Sign In
-      </button>
-      
-    </div>
-  </form>
- 
-</div>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
--- firebase-auth/src/components/Login.jsx
+++ firebase-auth/src/components/Login.jsx
@@ -1,61 +1,93 @@
-import React, { useState } from 'react'
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import React, { useState } from 'react';
+import { getAuth, signInWithEmailAndPassword,GoogleAuthProvider,signInWithPopup } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
+
 const Login = () => {
-   
-    const navigate = useNavigate();
-    const [email,setEmail] = useState("");
-    const [password,setPassword] = useState("")
-    const [user,setUser] = useState()
-    const [error,setError] = useState("")
-    const auth = getAuth();
-    const submit = (e)=>{
-        
-e.preventDefault()
+  const navigate = useNavigate();
 
-
-
-signInWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    navigate('/welcome', { replace: true });
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    setError(errorMessage)
-  });
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const auth = getAuth();
+  const provider = new GoogleAuthProvider();
+  const signInUser = async (email, password) => {
+    try {
+      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      const user = userCredential.user;
+      navigate('/welcome', { replace: true });
+    } catch (error) {
+      setError(error.message);
     }
+  };
+
+  const handleSignInWithGoogle = ()=>{
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        setUser(user);
+        navigate('/welcome', { replace: true });
+      })
+      .catch((error) => {
+        setError(error.message);
+        // Handle specific errors if needed
+      });
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    signInUser(email, password);
+  };
+
   return (
     <div className="w-full max-w-xs">
-      <h1 className='font-bold text-blue-700 text-xl'>Login</h1>
-  <form onSubmit={(e)=>submit(e)} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-    
-    <div className="mb-4">
-      <label className="block text-gray-700 text-sm font-bold mb-2" for="username">
-        Username
-      </label>
-      <input value={email} onChange={((e)=>setEmail(e.target.value))} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
+      <h1 className="font-bold text-blue-700 text-xl">Login</h1>
+      <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div className="mb-4">
+          <label htmlFor="username" className="block text-gray-700 text-sm font-bold mb-2">
+            Username
+          </label>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="username"
+            type="text"
+            placeholder="Username"
+          />
+        </div>
+        <div className="mb-6">
+          <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2">
+            Password
+          </label>
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            id="password"
+            type="password"
+            placeholder="******************"
+          />
+          <p className="text-red-500 text-xs italic">{error}</p>
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Sign In 
+          </button>
+          <button
+            onClick={handleSignInWithGoogle}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="button"
+            
+          >
+            Sign In with Google
+          </button>
+        </div>
+      </form>
     </div>
-    <div className="mb-6">
-      <label className="block text-gray-700 text-sm font-bold mb-2" for="password">
-        Password
-      </label>
-      <input value={password} onChange={((e)=>setPassword(e.target.value))} className="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" />
-      <p className="text-red-500 text-xs italic">{error}</p>
-    </div>
-    <div className="flex items-center justify-between">
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-        Sign In
-      </button>
-      
-    </div>
-  </form>
- 
-</div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
